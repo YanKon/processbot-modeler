4cb6d1c91d76c80989c30c592265d220d13786fe
Yannick <yannickkonrad@icloud.com>
2019-03-27 10:56:44 +0100

    changes

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 // import _ from 'lodash';
 import $ from 'jquery';
-import pizzaDiagram from '../resources/pizza-collaboration.bpmn';
+import newDiagram from '../resources/newDiagram.bpmn';
+import PizzaDiagram from '../resources/pizza-collaboration.bpmn';
+
 
 import BpmnModeler from 'bpmn-js/lib/Modeler';
 import propertiesPanelModule from 'bpmn-js-properties-panel';
 import propertiesProviderModule from 'bpmn-js-properties-panel/lib/provider/bpmn';
 import lintModule from 'bpmn-js-bpmnlint';
 import bpmnlintConfig from '../.bpmnlintrc';
+
+// import BpmnSearchProvider from 'bpmn-js/lib/features/search';
+// import BpmnKeyboardBindings from 'bpmn-js/lib/features/keyboard';
 
 import '../node_modules/bpmn-js/dist/assets/diagram-js.css';
 import '../node_modules/bpmn-js/dist/assets/bpmn-font/css/bpmn.css';
@@ -18,6 +23,7 @@ import '../node_modules/@fortawesome/fontawesome-free/js/all.js'
 
 var modeler = new BpmnModeler({
   container: '#js-canvas',
+  keyboard: { bindTo: document },
   linting: {
     bpmnlint: bpmnlintConfig
   },
@@ -31,12 +37,16 @@ var modeler = new BpmnModeler({
   ],
 });
 
-modeler.importXML(pizzaDiagram, function(err) {
+modeler.importXML(PizzaDiagram, function(err) {
 
   if (!err) {
     console.log('success!');
     modeler.get('canvas').zoom('fit-viewport');
-    modeler.get('linting').activateLinting();
+    zoomlevel = 0.8;	
+    modeler.get('canvas').zoom(zoomlevel);
+    // modeler.get('linting').activateLinting();
+    console.log(modeler.get('commandStack'));
+    // console.log(BpmnKeyboardBindings)
   } else {
     console.log('something went wrong:', err);
   }
@@ -46,16 +56,25 @@ $("#hidePanel").click(function() {
   $('#js-properties-panel').addClass("hidden");
   $('.io-editing-tools, .io-zoom-controls').css("right","15px");
   $('.io-zoom-controls').css("bottom","90px");
-  $('.bpmn-js-bpmnlint-button').css("right","125px");
+  $('.bpmn-js-bpmnlint-button').css("right","15px");
   $('#editPanel').removeClass("hidden");
+  // $('.io-alerts').css("left","50%");
+  $('.io-dialog .content').css("left","55%");
+  $('.djs-search-container').css("left","0");
+  
 });
 
 $("#editPanel").click(function() {
   $('#js-properties-panel').removeClass("hidden");
   $('.io-editing-tools, .io-zoom-controls').css("right","280px");
   $('.io-zoom-controls').css("bottom","15px");
-  $('.bpmn-js-bpmnlint-button').css("right","330px");
+  $('.bpmn-js-bpmnlint-button').css("right","280px");
   $('#editPanel').addClass("hidden");
+  // $('.io-alerts').css("left","40%");
+  $('.io-dialog .content').css("left","48%");
+  $('.djs-search-container').css("left","-180px");
+
+
 });
 
 $("#toggleFullscreen").click(function() {
@@ -63,7 +82,7 @@ $("#toggleFullscreen").click(function() {
 })
 
 function toggleFullscreen(elem) {
-  elem = elem || document.documentElement;
+  elem = document.documentElement;
   if (!document.fullscreenElement && !document.mozFullScreenElement &&
     !document.webkitFullscreenElement && !document.msFullscreenElement) {
     if (elem.requestFullscreen) {
@@ -86,4 +105,91 @@ function toggleFullscreen(elem) {
       document.webkitExitFullscreen();
     }
   }
+}
+
+var zoomlevel = 1;
+
+$("#zoomIn").on("click", function () {
+  if (zoomlevel <= 3.0) {
+    var canvas = modeler.get('canvas');	
+    zoomlevel = zoomlevel + 0.3;	
+    canvas.zoom(zoomlevel);
+  }
+
+})
+
+$("#zoomOut").on("click", function () {
+  if (zoomlevel >= 0.3) {
+    var canvas = modeler.get('canvas');	
+    zoomlevel = zoomlevel - 0.3;			
+
+    canvas.zoom(zoomlevel);
+  }
+})
+
+$("#zoomReset").on("click", function () {
+  modeler.get('canvas').zoom('fit-viewport');
+  var canvas = modeler.get('canvas');	
+  zoomlevel = 0.8;
+  canvas.zoom(0.8);
+})
+
+$(".close").on("click", function () {
+  $(".io-alerts").hide();
+})
+
+$("#showKeyboard").on("click", function () {
+  $(".io-dialog").toggle();
+})
+
+$("#createNew").on("click", function () {
+  openDiagram(newDiagram);
+})
+
+$("#openLocal").on("click", function () {
+  readSingleFile();
+})
+
+
+  
+
+
+function openDiagram(xml) {
+  modeler.importXML(xml, function(err) {
+
+    if (err) {
+      // container
+      //   .removeClass('with-diagram')
+      //   .addClass('with-error');
+
+      // container.find('.error pre').text(err.message);
+
+      console.error(err);
+    } else {
+      // container
+      //   .removeClass('with-error')
+      //   .addClass('with-diagram');
+    }
+  });
+  
+}
+
+
+function readSingleFile(e, callback) {
+  var file = e.target.files[0];
+
+  var reader = new FileReader();
+  reader.onload = function(){
+    var text = reader.result;
+    openDiagram(text);
+    $(".canvas").removeClass("hidden");
+    $(".properties-panel-parent").removeClass("hidden");
+    $(".buttons").removeClass("hidden");
+
+    $(".content").addClass("hidden");
+    e.stopPropagation();
+    e.preventDefault();
+  };
+  reader.readAsText(file);
+
 }
