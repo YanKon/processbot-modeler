edfee5cb222600570d18f23e79beffcbedfe6748
Yannick <yannickkonrad@icloud.com>
2019-04-04 16:22:30 +0200

    gateways mit chatbot instructions

--- properties-panel-extension/provider/chatbot/parts/DetailInstructionProps.js
+++ properties-panel-extension/provider/chatbot/parts/DetailInstructionProps.js
@@ -9,16 +9,7 @@ export default function(group, element) {
 
   // Only return an entry, if the currently selected
   // element is a start event.
-  if (is(element, 'bpmn:Task')) {
-    group.entries.push(entryFactory.textBox({
-      id : 'detailInstruction',
-      description : 'Give detail instructions',
-      label : 'Detail instructions',
-      modelProperty : 'detailInstruction'
-    }));
-  }
-
-  if (is(element, 'bpmn:Process')) {
+  if (is(element, 'bpmn:Task') || is(element, 'bpmn:Gateway')) {
     group.entries.push(entryFactory.textBox({
       id : 'detailInstruction',
       description : 'Give detail instructions',
--- properties-panel-extension/provider/chatbot/parts/InstructionProps.js
+++ properties-panel-extension/provider/chatbot/parts/InstructionProps.js
@@ -9,15 +9,7 @@ export default function(group, element) {
 
   // Only return an entry, if the currently selected
   // element is a start event.
-  if (is(element, 'bpmn:Task')) {
-    group.entries.push(entryFactory.textBox({
-      id : 'instruction',
-      description : 'Give instructions',
-      label : 'Instructions',
-      modelProperty : 'instruction'
-    }));
-  }
-  if (is(element, 'bpmn:Process')) {
+  if (is(element, 'bpmn:Task') || is(element, 'bpmn:Gateway')) {
     group.entries.push(entryFactory.textBox({
       id : 'instruction',
       description : 'Give instructions',
